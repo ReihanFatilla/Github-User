f2556c47ee33f86e0bda670fad4a08b00f6fbab6
ReihanFatilla <reihanfatilla@gmail.com>
2022-09-07 20:25:00 +0700

    [feat]: finished json utils function to get List of user

---
+++ app/src/main/java/com/reift/githubuser/data/JsonUtils.kt
@@ -0,0 +1,30 @@
+package com.reift.githubuser.data
+
+import android.content.Context
+import com.reift.githubuser.R
+import com.reift.githubuser.model.User
+import org.json.JSONArray
+import org.json.JSONObject
+
+object JsonUtils {
+    fun getGithubUser(ctx: Context): List<User> {
+        val result = ArrayList<User>()
+        val json = JSONObject(ctx.resources.openRawResource(R.raw.githubuser).bufferedReader().readText())
+        val jsonArray = json.getJSONArray("users")
+
+        for(i in 0 until jsonArray.length()){
+            val jsonObjectData = jsonArray.getJSONObject(i)
+            val user = User(
+                jsonObjectData.getString("username"),
+                jsonObjectData.getString("name"),
+                jsonObjectData.getString("company"),
+                jsonObjectData.getString("location"),
+                jsonObjectData.getInt("repository"),
+                jsonObjectData.getInt("follower"),
+                jsonObjectData.getInt("following"),
+            )
+            result.add(user)
+        }
+        return result
+    }
+}
