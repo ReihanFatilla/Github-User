70b24ed3ee300082c04b15634e03af63288845cd
ReihanFatilla <reihanfatilla@gmail.com>
2022-09-07 20:10:13 +0700

    [feat]: added model

---
+++ app/src/main/java/com/reift/githubuser/model/User.kt
@@ -0,0 +1,15 @@
+package com.reift.githubuser.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    val username: String,
+    val name: String,
+    val company: String,
+    val location: String,
+    val repository: Int,
+    val follower: Int,
+    val following: Int
+): Parcelable
