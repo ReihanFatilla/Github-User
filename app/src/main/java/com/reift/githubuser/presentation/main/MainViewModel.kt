defb9491327cbe85ca9f1173e59b2b813dea1db5
ReihanFatilla <reihanfatilla@gmail.com>
2022-09-08 09:22:23 +0700

    [feat]: finished main viewmodel

--- app/src/main/java/com/reift/githubuser/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/reift/githubuser/presentation/main/MainViewModel.kt
@@ -1,6 +1,21 @@
 package com.reift.githubuser.presentation.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.reift.githubuser.data.UserRepository
+import com.reift.githubuser.data.network.response.search.ItemsItem
+import com.reift.githubuser.data.network.response.search.UserResponse
 
 class MainViewModel: ViewModel() {
+    private val repository = UserRepository()
+
+    val userResponse = MutableLiveData<List<ItemsItem>>()
+
+    fun searchByUsername(username: String){
+        repository.searchByUsername(
+            username
+        ) {
+            userResponse.value = it.items
+        }
+    }
 }
