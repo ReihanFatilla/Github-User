eb89d2a89e252168e34eb47d928c55e5d728e425
ReihanFatilla <reihanfatilla@gmail.com>
2022-09-08 07:10:03 +0700

    [feat]: cleaning up project on warning

--- app/src/main/java/com/reift/githubuser/DetailActivity.kt
+++ app/src/main/java/com/reift/githubuser/DetailActivity.kt
@@ -1,8 +1,8 @@
 package com.reift.githubuser
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.reift.githubuser.databinding.ActivityDetailBinding
 import com.reift.githubuser.model.User
@@ -19,62 +19,56 @@ class DetailActivity : AppCompatActivity() {
 
         val userDetail = intent.getParcelableExtra<User>(EXTRA_DETAIL)
 
-        setUpButton()
         setUpDetailView(userDetail)
-    }
-
-    private fun setUpButton() {
-        binding.apply {
-
-        }
     }
 
     private fun setUpDetailView(user: User?) {
 
-            binding.apply {
-                if (user != null){
+        binding.apply {
+            if (user != null) {
 
-                    with(user){
-                        tvName.text = name
-                        tvUsername.text = username
-                        tvLocation.text = location
-                        tvCompany.text = company
-                        tvUserFollowers.text = follower.toString()
-                        tvUserFollowing.text = following.toString()
-                        tvUserRepository.text = repository.toString()
+                with(user) {
+                    tvName.text = name
+                    tvUsername.text = username
+                    tvLocation.text = location
+                    tvCompany.text = company
+                    tvUserFollowers.text = follower.toString()
+                    tvUserFollowing.text = following.toString()
+                    tvUserRepository.text = repository.toString()
 
-                        Glide.with(applicationContext)
-                            .load(avatar)
-                            .into(imgUser)
+                    Glide.with(applicationContext)
+                        .load(avatar)
+                        .into(imgUser)
 
 
-                        btnShare.setOnClickListener {
-                            val textValue = "This is ${user.name}'s GitHub Profile\n" +
-                                    "Username: ${user.username}\n" +
-                                    "Company: ${user.company}\n" +
-                                    "Location: ${user.location}\n" +
-                                    "Total ${user.repository} Repositories\n" +
-                                    "${user.follower} Followers & ${user.following} Following"
+                    btnShare.setOnClickListener {
+                        val textValue = "This is ${user.name}'s GitHub Profile\n" +
+                                "Username: ${user.username}\n" +
+                                "Company: ${user.company}\n" +
+                                "Location: ${user.location}\n" +
+                                "Total ${user.repository} Repositories\n" +
+                                "${user.follower} Followers & ${user.following} Following"
 
-                            val sendIntent: Intent = Intent().apply {
-                                action = Intent.ACTION_SEND
-                                putExtra(Intent.EXTRA_TEXT, textValue)
-                                type = "text/plain"
-                            }
-
-                            val shareIntent = Intent.createChooser(sendIntent, getString(R.string.txt_share_msg))
-                            startActivity(shareIntent)
+                        val sendIntent: Intent = Intent().apply {
+                            action = Intent.ACTION_SEND
+                            putExtra(Intent.EXTRA_TEXT, textValue)
+                            type = "text/plain"
                         }
+
+                        val shareIntent =
+                            Intent.createChooser(sendIntent, getString(R.string.txt_share_msg))
+                        startActivity(shareIntent)
                     }
                 }
-                btnBack.setOnClickListener {
-                    finish()
-                }
             }
+            btnBack.setOnClickListener {
+                finish()
+            }
+        }
 
     }
 
-    companion object{
+    companion object {
         const val EXTRA_DETAIL = "INTENT_EXTRA_DETAIL"
     }
 }
