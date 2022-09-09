9af96922703d1a9f0dfe50b71080b870150d90b5
ReihanFatilla <reihanfatilla@gmail.com>
2022-09-09 14:59:59 +0700

    [feat]: added all business logic to repository

--- app/src/main/java/com/reift/githubuser/data/UserRepository.kt
+++ app/src/main/java/com/reift/githubuser/data/UserRepository.kt
@@ -1,6 +1,11 @@
 package com.reift.githubuser.data
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import com.reift.githubuser.data.local.LocalDataSource
+import com.reift.githubuser.data.local.datastore.ThemePreferences
+import com.reift.githubuser.data.local.room.UserDB
 import com.reift.githubuser.data.local.room.UserEntity
 import com.reift.githubuser.data.network.ApiConfig
 import com.reift.githubuser.data.network.ApiService
@@ -10,10 +15,12 @@ import com.reift.githubuser.data.network.response.search.UserResponse
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class UserRepository private constructor(
-    private val apiService: ApiService,
-    private val localDataSource: LocalDataSource
-){
+class UserRepository(context: Context, dataStore: DataStore<Preferences>){
+
+    private val apiService: ApiService = ApiConfig.getApiService()
+    private val dao = UserDB.getInstance(context).userDao()
+    private val theme = ThemePreferences.getInstance(dataStore)
+    private val localDataSource: LocalDataSource = LocalDataSource.getInstance(dao, theme)
 
     fun insertFavorite(user: UserEntity) = localDataSource.insertFavorite(user)
 
@@ -79,18 +86,6 @@ class UserRepository private constructor(
                     errorHandler(it)
                 }
             )
-    }
-
-    companion object {
-        @Volatile
-        private var instance: UserRepository? = null
-
-        fun getInstance(apiService: ApiService, localDataSource: LocalDataSource): UserRepository =
-            instance ?: synchronized(this) {
-                instance ?: UserRepository(apiService, localDataSource).apply {
-                    instance = this
-                }
-            }
     }
 
 }
