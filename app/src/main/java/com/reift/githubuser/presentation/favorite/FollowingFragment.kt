f57d078c19ab7b43c2fb7cdc0244333c60d1e240
ReihanFatilla <reihanfatilla@gmail.com>
2022-09-11 20:50:11 +0700

    [feat]: added following fragment ui

--- app/src/main/java/com/reift/githubuser/presentation/favorite/FollowingFragment.kt
+++ app/src/main/java/com/reift/githubuser/presentation/favorite/FollowingFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.reift.githubuser.R
-class FavoriteFragment : Fragment() {
+class FollowingFragment : Fragment() {
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/reift/githubuser/presentation/favorite/FollowingViewModel.kt
+++ app/src/main/java/com/reift/githubuser/presentation/favorite/FollowingViewModel.kt
@@ -3,5 +3,5 @@ package com.reift.githubuser.presentation.favorite
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 
-class FavoriteViewModel(application: Application): AndroidViewModel(application) {
+class FollowingViewModel(application: Application): AndroidViewModel(application) {
 }
