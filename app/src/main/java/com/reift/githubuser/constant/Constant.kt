026138bc891d8d21700ce0c03b849a3c62551e65
ReihanFatilla <reihanfatilla@gmail.com>
2022-09-11 20:08:26 +0700

    [feat]: added Shared Preferences Helper

--- app/src/main/java/com/reift/githubuser/constant/Constant.kt
+++ app/src/main/java/com/reift/githubuser/constant/Constant.kt
@@ -4,13 +4,15 @@ import androidx.datastore.preferences.core.booleanPreferencesKey
 
 object Constant {
     const val EXTRA_DETAIL = "INTENT_EXTRA_DETAIL"
-
     const val BUNDLE_FOLLOW = "BUNDLE_FOLLOW"
+
     const val BUNDLE_USERNAME = "BUNDLE_USERNAME"
-
     const val TYPE_FOLLOWERS = "TYPE_FOLLOWERS"
-    const val TYPE_FOLLOWING = "TYPE_FOLLOWING"
 
+    const val TYPE_FOLLOWING = "TYPE_FOLLOWING"
     const val PREF_SETTINGS = "SETTINGS"
+
     const val PREF_THEME = "THEME_SETTINGS"
+
+    const val PREF_USER = "PREF_USER"
 }
