9bba030cf3451d4371e90bc5e4751b4eed31e973
ReihanFatilla <reihanfatilla@gmail.com>
2022-09-14 11:16:56 +0700

    [feat]: added detail use case and entity

---
+++ core/src/main/java/com/reift/core/domain/usecase/detail/DetailUseCase.kt
@@ -0,0 +1,11 @@
+package com.reift.core.domain.usecase.detail
+
+import com.reift.core.domain.entity.detail.Detail
+import com.reift.core.domain.entity.detail.follow.Follow
+import io.reactivex.rxjava3.core.Flowable
+
+interface DetailUseCase {
+    fun getUserDetail(username: String): Flowable<Detail>
+    fun getUserFollowing(username: String): Flowable<List<Follow>>
+    fun getUserFollowers(username: String): Flowable<List<Follow>>
+}
