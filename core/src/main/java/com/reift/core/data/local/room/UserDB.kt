865307048fb8f95ac3b41e1d071c9b562dcdbc54
ReihanFatilla <reihanfatilla@gmail.com>
2023-03-26 11:26:33 +0700

    [feat]: added exportSchema false on userDB

--- core/src/main/java/com/reift/core/data/local/room/UserDB.kt
+++ core/src/main/java/com/reift/core/data/local/room/UserDB.kt
@@ -3,7 +3,7 @@ package com.reift.core.data.local.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [UserEntity::class], version = 1, exportSchema = false)
+@Database(entities = [UserEntity::class], version = 1, exportSchema = false )
 abstract class UserDB: RoomDatabase() {
     abstract fun userDao(): UserDao
 }
